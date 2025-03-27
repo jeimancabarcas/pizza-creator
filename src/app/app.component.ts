6bce8eb9136fd3578f0f86f581692ede56e62775
Jeiman Cabarcas <jeimancabarcas@gmail.com>
2025-03-27 09:43:36 -0500

    fix module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PizzaAppModule } from './pizza-app/pizza-app.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ 
+    PizzaAppModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
